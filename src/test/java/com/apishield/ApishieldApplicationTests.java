6e512343b5d58c84aa9851f47d2d5c0e58f378ca
Mohd16kaif <kaif39882@gmail.com>
2025-09-02 11:30:12 +0530

    Initial commit for backend

--- src/test/java/com/apishield/ApishieldApplicationTests.java
+++ src/test/java/com/apishield/ApishieldApplicationTests.java
@@ -2,13 +2,13 @@ package com.apishield;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // This tells Spring to use application-test.properties
 class ApishieldApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
